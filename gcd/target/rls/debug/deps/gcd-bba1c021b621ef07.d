0a2015114be98659b4205df400562d3a24baa35a
abdsh <abd.shobi@gmail.com>
2022-01-22 20:14:42 +0330

    actix-gcd:web ap for computing the greatest commen divisor

---
+++ gcd/target/rls/debug/deps/gcd-bba1c021b621ef07.d
@@ -0,0 +1,5 @@
+/home/shoobi/Desktop/rust_proj/programming_rust/gcd/target/rls/debug/deps/gcd-bba1c021b621ef07.rmeta: src/main.rs
+
+/home/shoobi/Desktop/rust_proj/programming_rust/gcd/target/rls/debug/deps/gcd-bba1c021b621ef07.d: src/main.rs
+
+src/main.rs:
